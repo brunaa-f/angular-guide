7f6a706d9c5fe4a5d20adfe7e6e90a43e0b779ff
Bruna Fernandes <109226923+brunaa-f@users.noreply.github.com>
2024-06-29 16:58:26 -0300

    outputting user-specific tasks

--- 01-starting-project/src/app/tasks/tasks.component.ts
+++ 01-starting-project/src/app/tasks/tasks.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
+import { TitleStrategy } from '@angular/router';
 
 @Component({
     selector: 'app-tasks',
@@ -9,5 +10,13 @@ import { TaskComponent } from "./task/task.component";
     imports: [TaskComponent]
 })
 export class TasksComponent {
- @Input() name: string | undefined ;
+ @Input({required: true}) userId!: string;
+ @Input({required: true}) name!: string;
+
+ tasks = [
+  {id: 't1', userId: 'u1', title: 'Master angular', summary: 'learn all the basic and advanced features of Angular & how to apply them.', dueDate: '2025-12-31' }]
+
+  get selectedUserTasks() {
+    return this.tasks.filter((task) => task.userId === this.userId);
+  }
 }
