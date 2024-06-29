4a89b31d3a3bc2d347c0f66176e439603490047c
Bruna Fernandes <109226923+brunaa-f@users.noreply.github.com>
2024-06-29 13:04:21 -0300

    interfaces

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,5 +1,11 @@
 import { Component, EventEmitter, Input, Output, input} from '@angular/core';
 
+interface User {
+  id: string;
+  avatar: string;
+  name: string;
+}
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -8,17 +14,15 @@ import { Component, EventEmitter, Input, Output, input} from '@angular/core';
 })
 
 export class UserComponent {
-  @Input({required: true}) id!: string;
-  @Input({required: true}) avatar!: string; 
-  @Input({required: true}) name!: string; 
+  @Input({required: true}) user!: User;
   @Output() select = new EventEmitter<string>()
   //select = output<string>();
 
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
 
   onSelectedUser() {
-    this.select.emit(this.id)
+    this.select.emit(this.user.id)
   }
 }
