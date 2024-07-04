ec69e23ce89c2192aa13b32e166982f80024ddcc
Bruna Fernandes <109226923+brunaa-f@users.noreply.github.com>
2024-07-04 10:19:13 -0300

    using localStorage for data storage

--- 01-starting-project/src/app/tasks/tasks.service.ts
+++ 01-starting-project/src/app/tasks/tasks.service.ts
@@ -8,6 +8,14 @@ export class TasksService {
         { id: 't1', userId: 'u1', title: 'Master angular', summary: 'learn all the basic and advanced features of Angular & how to apply them.', dueDate: '2025-12-31' }
     ]
 
+    constructor () {
+        const tasks = localStorage.getItem('tasks')
+
+        if (tasks) {
+            this.tasks = JSON.parse(tasks);
+        }
+    }
+
     getUserTasks(userId: string) {
         return this.tasks.filter((task) => task.userId === userId);
     }
@@ -20,9 +28,15 @@ export class TasksService {
             summary: taskData.summary,
             dueDate: taskData.date
         })
+        this.saveTasks()
     }
 
     removeTask(id: string) {
         this.tasks = this.tasks.filter((task) => task.id !== id )
+        this.saveTasks()
+    }
+
+    private saveTasks () {
+        localStorage.setItem('tasks', JSON.stringify(this.tasks))
     }
 }
