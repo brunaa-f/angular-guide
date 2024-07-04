9a1c446f25a23e864240749a73229db94998d526
Bruna Fernandes <109226923+brunaa-f@users.noreply.github.com>
2024-07-04 09:45:36 -0300

    alternative dependency injection

--- 01-starting-project/src/app/tasks/tasks.component.ts
+++ 01-starting-project/src/app/tasks/tasks.component.ts
@@ -32,11 +32,7 @@ export class TasksComponent {
     this.isAddingTask = true;
   }
 
-  onCancelAddTask() {
+  onCloseAddTask() {
     this.isAddingTask = false
-  }
-
-  onAddTask(taskData: NewTaskData) {
-    this.isAddingTask = false;
   }
 }
