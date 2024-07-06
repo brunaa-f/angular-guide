267c5ab053f5f7995c4d531af75412ff63a7e8a0
Bruna Fernandes <109226923+brunaa-f@users.noreply.github.com>
2024-07-06 14:05:29 -0300

    declaring and using components

--- 01-starting-project/src/app/app.module.ts
+++ 01-starting-project/src/app/app.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from "@angular/core";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { UserComponent } from "./user/user.component";
+import { TasksComponent } from "./tasks/tasks.component";
+import { BrowserModule } from "@angular/platform-browser";
 
 @NgModule({
-    declarations: []  //declarar e registrarv todos os components que trabalham juntos
+    declarations: [AppComponent],   //declarar e registrarv todos os components que trabalham juntos
+    bootstrap: [AppComponent],
+    imports: [BrowserModule, HeaderComponent,UserComponent, TasksComponent]
 })
 export class AppModule {
 
