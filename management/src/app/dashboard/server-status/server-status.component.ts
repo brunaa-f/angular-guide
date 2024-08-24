98301ff426704faa3f31e9cc4623b0561f785b6c
Bruna Fernandes <169090978+brunaaaf@users.noreply.github.com>
2024-08-24 10:50:35 -0300

    cleanup with destroyRef

--- management/src/app/dashboard/server-status/server-status.component.ts
+++ management/src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, DestroyRef, inject, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,15 +7,15 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent implements OnInit, OnDestroy {
+export class ServerStatusComponent implements OnInit {
   currentStatus: 'online' | 'offline' | 'unknown' = 'offline';
-  private interval?: ReturnType<typeof setInterval>;
+  private destroyRef = inject(DestroyRef)
 
   constructor() {
   }
 
   ngOnInit() {
-    this.interval = setInterval(()=> {
+    const interval = setInterval(()=> {
       const rnd = Math.random(); 
 
       if (rnd < 0.5) {
@@ -26,10 +26,14 @@ export class ServerStatusComponent implements OnInit, OnDestroy {
         this.currentStatus = 'unknown'
       }
     }, 5000);
+
+    this.destroyRef.onDestroy(()=> {
+      clearInterval(interval);
+    })
   }
 
-  ngOnDestroy(): void {
-    clearTimeout(this.interval)
-  }
+  // ngOnDestroy(): void {
+  //   clearTimeout(this.interval)
+  // }
 
 }
