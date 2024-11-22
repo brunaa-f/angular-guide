cc2004684cbae1cfa85abe5ba98422dc9824fe6d
Bruna Fernandes <109226923+brunaa-f@users.noreply.github.com>
2024-11-22 15:05:38 -0300

    feat: styling active navigation links

--- routes/.angular/cache/18.2.10/routing/vite/deps/@angular_core.js
+++ routes/.angular/cache/18.2.10/routing/vite/deps/@angular_core.js
@@ -461,7 +461,7 @@ import {
   ɵɵvalidateIframeAttribute,
   ɵɵviewQuery,
   ɵɵviewQuerySignal
-} from "./chunk-FNWGKWM6.js";
+} from "./chunk-HCNZ5MW6.js";
 export {
   ANIMATION_MODULE_TYPE,
   APP_BOOTSTRAP_LISTENER,
